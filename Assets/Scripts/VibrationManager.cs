9e29fdebcc9b1dc1e28d072a922b81ceedf19e70
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-06-24 17:14:40 +0900

    Vive Controller振動実装

---
+++ Assets/Scripts/VibrationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VibrationManager : MonoBehaviour
+{
+    public SteamVR_Input_Sources controller;
+    public SteamVR_Action_Vibration vibration;
+    public SteamVR_Action_Boolean trigger;
+    public float secStart;
+    public float secEnd;
+    public float frequency;
+    public float strength;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.GetState(controller))
+        {
+            vibration.Execute(secStart, secEnd, frequency, strength, controller);
+        }
+    }
+}
